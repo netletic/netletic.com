42472216418e7b3ee0cb9bdf45eb5e77247f01ff
netletic <netletic@gmail.com>
2021-04-10 11:05:51 +0200

    added Path and urlretrieve() for t1.txt and t2.txt

--- find_the_loop/loop.py
+++ find_the_loop/loop.py
@@ -1,17 +1,35 @@
 from collections import Counter
+from pathlib import Path
 from pprint import pprint
 from typing import List
 from typing import Tuple
+from urllib.request import urlretrieve
 
 
-def get_rx_bytes(filename: str) -> Counter:
+tmp = Path("/tmp")
+t1_url = (
+    "https://raw.githubusercontent.com/netletic/netletic.com/main/find_the_loop/t1.txt"
+)
+t2_url = (
+    "https://raw.githubusercontent.com/netletic/netletic.com/main/find_the_loop/t2.txt"
+)
+t1_file = tmp / "t1.txt"
+t2_file = tmp / "t2.txt"
+
+if not t1_file.exists():
+    urlretrieve(t1_url, t1_file)
+if not t2_file.exists():
+    urlretrieve(t2_url, t2_file)
+
+
+def get_rx_bytes(file: Path) -> Counter:
     rx = {}
-    with open(filename) as fp:
+    with open(file) as fp:
         for line in fp.readlines():
             if "Chassis/Slot/Port" in line:
                 _, port, _ = line.split()
                 continue
-            if "Bytes Received" in line:
+            elif "Bytes Received" in line:
                 _, bytes_rx, *_ = line.split(":")
                 bytes_rx = bytes_rx.replace(", Unicast Frames ", "").lstrip()
                 rx[port] = int(bytes_rx)
@@ -22,10 +40,11 @@ def delta(t1: Counter, t2: Counter) -> List[Tuple[str, int]]:
     return (t2 - t1).most_common(5)
 
 
-def main():
-    t1 = get_rx_bytes("t1.txt")
-    t2 = get_rx_bytes("t2.txt")
+def main() -> int:
+    t1 = get_rx_bytes(t1_file)
+    t2 = get_rx_bytes(t2_file)
     pprint(delta(t1, t2))
+    return 0
 
 
 if __name__ == "__main__":
